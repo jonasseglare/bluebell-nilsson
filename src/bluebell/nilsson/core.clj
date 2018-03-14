d36db06d32e27026d94767bffc9d1344de4b10ca
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-03-14 23:37:59 +0100

    good

--- src/bluebell/nilsson/core.clj
+++ src/bluebell/nilsson/core.clj
@@ -1,6 +1,75 @@
-(ns bluebell-nilsson.core)
+(ns bluebell.nilsson.core
+  (:require [symbol-analyzer.core :as analyzer]
+            [clojure.walk :as walk]
+            [clojure.spec.alpha :as spec]
+            [symbol-analyzer.extraction :as extraction]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn except [x]
+  x)
+
+(defmacro either [& symbols]
+  (assert (every? symbol? symbols))
+  `(or ~@(map (fn [x] `(except ~x)) symbols)))
+
+(defn is-except? [x]
+  (let [v (-> x meta :symbol-info :var)]
+    (= (var except) v)))
+
+(spec/def ::except (spec/cat :prefix is-except?
+                              :value symbol?))
+
+(spec/def ::subexpr (spec/or :except ::except
+                             :coll coll?
+                             :any any?))
+
+(defn flatten? [x]
+  (let [t (first (spec/conform ::subexpr x))]
+    (= :coll t)))
+
+(defn deep-flatten-into [dst x]
+  (if (flatten? x)
+    (reduce deep-flatten-into dst x)
+    (conj dst x)))
+
+(defn deep-flatten [x]
+    (deep-flatten-into [] x))
+
+(defn disp [x]
+  x)
+
+(defn analyze-symbols [expr symbol-transducer]
+  (transduce
+   (comp (filter symbol?)
+         symbol-transducer)
+   conj
+   []
+   (-> expr
+       analyzer/analyze-sexp
+       deep-flatten
+       disp)))
+
+(defn unknown-symbols [expr]
+  (set
+   (analyze-symbols
+    expr
+    (comp  (map (fn [sym]
+                  {:sym sym
+                   :type (-> sym meta :symbol-info :type)}))
+           (filter (fn [info]
+                     (-> info
+                         :type
+                         (= :unknown))))
+           (map :sym)))))
+
+(defmacro unknown-symbols-here [expr]
+  `(quote ~(unknown-symbols expr)))
+
+
+
+
+
+
+#_(defn known-symbols [expr]
+  (->> expr
+       vals
+       ))
--- test/bluebell/nilsson/core_test.clj
+++ test/bluebell/nilsson/core_test.clj
@@ -1,7 +1,15 @@
-(ns bluebell-nilsson.core-test
-  (:require [clojure.test :refer :all]
-            [bluebell-nilsson.core :refer :all]))
+(ns bluebell.nilsson.core-test
+  (:require [symbol-analyzer.core :refer [analyze-sexp]]
+            [clojure.test :refer :all]
+            [bluebell.nilsson.core :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= #{'b 'c}
+           (unknown-symbols '(let [a 9] (+ a b c)))))
+    (is (= #{'a 'b 'c}
+           (unknown-symbols '(do a (let [a 9] (+ a b c))))))
+    (is (= #{'b}
+           (unknown-symbols '(let [a 9] (+ a b (bluebell.nilsson.core/except c))))))
+    (is (= #{'a 'b 'd 'e}
+           (unknown-symbols-here (+ a b (except c) (d e)))))))
