412fa246a87a84dbe867608ab254185b2d6c1671
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-03-14 22:12:04 +0100

    good

---
+++ test/bluebell/nilsson/core_test.clj
@@ -0,0 +1,7 @@
+(ns bluebell-nilsson.core-test
+  (:require [clojure.test :refer :all]
+            [bluebell-nilsson.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
